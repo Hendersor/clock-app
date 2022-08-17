9c850d8a56b7177226c85a3905c08d677bd2bd74
Francisco <montejof1998@gmail.com>
2022-08-16 18:09:01 -0700

    Font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      black: "#000000",
+      gray: "#303030",
+      white: "#FFFFFF",
+    },
+    fontFamily: {
+      Inter: ["Inter", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],

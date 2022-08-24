3d4ee16d34b7e67f24f08f66090a62894d4d4e26
Francisco <montejof1998@gmail.com>
2022-08-24 02:13:27 -0700

    2 of the 3 API's are working and showing the data

---
+++ src/index.js
@@ -0,0 +1,49 @@
+"use strict";
+//1. Conectarse a la API y retornar el valor deseado
+//2. Usar ese valor en la otra coneccion de la API
+//3. Una vez conectado desplegar esa data y modificarla
+const apiLocation =
+  "https://api.ipbase.com/v2/info?apikey=0CaYR2iagldHav1ltslfW8vJbyP3yW3swiDit7Dy&ip=1.1.1.1";
+
+const apiTime = "http://worldtimeapi.org/api/timezone";
+
+const conectionAPI = async (apiLocation, apiTime) => {
+  const responseLocation = await fetch(apiLocation);
+  const dataLocation = await responseLocation.json();
+  const location = dataLocation.data.timezone.id;
+
+  const apiTimeURL = `${apiTime}/${location}`;
+  const resposeTime = await fetch(apiTimeURL);
+  const dataTime = await resposeTime.json();
+
+  deployMainData(dataTime, dataLocation);
+  //   deployMoreData(dataTime, dataLocation);
+};
+
+(async () => {
+  console.log(await conectionAPI(apiLocation, apiTime));
+})();
+
+function deployMainData(dataTime, dataLocation) {
+  console.log(dataLocation, dataLocation);
+  const abbreviation = document.getElementById("abbreviation");
+  abbreviation.innerText = dataTime.abbreviation;
+
+  const state = document.getElementById("state");
+  state.innerText = dataLocation.data.location.region.name.toUpperCase();
+
+  const country = document.getElementById("country");
+  country.innerText = dataLocation.data.location.country.alpha3;
+}
+
+function deployMoreData(dataTime, dataLocation) {
+  console.log(dataTime);
+  const timeZone = document.getElementById("timeZone");
+  timeZone.innerText = `${dataLocation.data.location.continent.name}/${dataLocation.data.location.country.name}`;
+
+  const dayOfTheYear = document.getElementById("dayOfTheYear");
+  dayOfTheYear.innerText = dataTime.day_of_year;
+
+  const dayOfTheWeek = document.getElementById("dayOfTheWeek");
+  dayOfTheWeek.innerText = dataTime.day_of_week;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
       white: "#FFFFFF",
       data: "#ecf0f1",
       bg: "rgba(49, 53, 50, 0.36)",
+      test: "#234509",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      mymd: "962px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     fontFamily: {
       Inter: ["Inter", "sans-serif"],
@@ -16,6 +25,8 @@ module.exports = {
       backgroundImage: {
         dayMobile:
           "url('https://res.cloudinary.com/dwdz4mn27/image/upload/v1660698705/clock-app/starter-code/assets/mobile/bg-image-daytime_e4i4o3.jpg')",
+        dayTablet:
+          "url('https://res.cloudinary.com/dwdz4mn27/image/upload/v1660698706/clock-app/starter-code/assets/tablet/bg-image-daytime_q13f3y.jpg')",
       },
     },
   },

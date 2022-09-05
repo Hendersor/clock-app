08352d3c62303ba5d55dd487a491124a9cc9a163
Francisco <montejof1998@gmail.com>
2022-09-05 13:32:35 -0700

    We get the user's ip so we can see the location and use it to show the datetime

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 "use strict";
-//1. Conectarse a la API y retornar el valor deseado
-//2. Usar ese valor en la otra coneccion de la API
-//3. Una vez conectado desplegar esa data y modificarla
+
+//Sacar la IP
+//Usar la IP para sacar la ubicacion
+//Usar esa ubicacion para sacar el tiempo de la zona
 const apiLocation =
-  "https://api.ipbase.com/v2/info?apikey=0CaYR2iagldHav1ltslfW8vJbyP3yW3swiDit7Dy&ip=1.1.1.1";
+  "https://api.ipbase.com/v2/info?apikey=0CaYR2iagldHav1ltslfW8vJbyP3yW3swiDit7Dy&ip=";
 
 const apiTime = "http://worldtimeapi.org/api/timezone";
 const apiQuotes = "https://programming-quotes-api.herokuapp.com/Quotes/random";
+const apiKey = "884e36038e2965a033f2f42b1f5f883da868c95060b39832b73d9974";
 
 const quoteApiConection = async () => {
-  console.log("Btn presionado");
   const quote = document.getElementById("quote");
   const author = document.getElementById("author");
   const responseQuote = await fetch(apiQuotes);
@@ -17,21 +18,31 @@ const quoteApiConection = async () => {
   quote.innerText = quoteData.en;
   author.innerText = quoteData.author;
 };
+quoteApiConection();
 
 const refreshIcon = document.getElementById("refreshIcon");
 refreshIcon.addEventListener("click", quoteApiConection);
 
 const conectionAPI = async (apiLocation, apiTime) => {
-  const responseLocation = await fetch(apiLocation);
-  const dataLocation = await responseLocation.json();
-  const location = dataLocation.data.timezone.id;
+  const getData = await fetch(`https://api.ipdata.co?api-key=${apiKey}`);
+  const userIp = await getData.json();
+  console.log(userIp.ip); // Obtener la IP del usuario
 
+  // const responseLocation = await fetch(`${apiLocation}${userIp.ip}`);
+  // const dataLocation = await responseLocation.json();
+  // const userLocation = dataLocation.data.timezone.id;
+  //Por medio de la IP obtener la ubicacion del usuario
+
+  const apiTimeConection = await fetch(`${apiTime}/America/Mexico_City`);
+  const apiDataTime = await apiTimeConection.json(); //Obtenemos los datos de la zona horaria del usuario
+  console.log(apiDataTime);
+
+  const location = apiDataTime.timezone;
   const apiTimeURL = `${apiTime}/${location}`;
   const resposeTime = await fetch(apiTimeURL);
   const dataTime = await resposeTime.json();
-
-  deployMainData(dataTime, dataLocation);
-  //   deployMoreData(dataTime, dataLocation);
+  // deployMainData(dataTime, dataLocation);
+  // deployMoreData(dataTime, dataLocation);
 };
 
 (async () => {
@@ -39,7 +50,6 @@ const conectionAPI = async (apiLocation, apiTime) => {
 })();
 
 function deployMainData(dataTime, dataLocation) {
-  console.log(dataLocation, dataLocation);
   const abbreviation = document.getElementById("abbreviation");
   abbreviation.innerText = dataTime.abbreviation;
 
@@ -51,7 +61,6 @@ function deployMainData(dataTime, dataLocation) {
 }
 
 function deployMoreData(dataTime, dataLocation) {
-  console.log(dataTime);
   const timeZone = document.getElementById("timeZone");
   timeZone.innerText = `${dataLocation.data.location.continent.name}/${dataLocation.data.location.country.name}`;
 
